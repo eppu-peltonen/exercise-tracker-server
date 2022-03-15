16462081597b5087b7b3edc3e35ca995b74662b6
Eppu Peltonen <eppu.peltonen@icloud.com>
2022-03-15 21:45:40 +0200

    db testi toimii

--- controllers/exercises.js
+++ controllers/exercises.js
@@ -3,10 +3,10 @@ const { Pool } = require('pg')
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: true
+  ssl: {rejectUnauthorized: false}
 })
 
-exerciseRouter.get('/', async (req, res) => {
+exerciseRouter.get('/db', async (req, res) => {
   try {
     const client = await pool.connect()
     const result = await client.query('SELECT * FROM test_table')
@@ -15,7 +15,7 @@ exerciseRouter.get('/', async (req, res) => {
     client.release()
   } catch (err) {
     console.error(err)
-    res.send('Error', err)
+    res.send('Error ' + err)
   }
 })
 
