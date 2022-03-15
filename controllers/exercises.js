460d093097148f2a47464b889286fc0d95a0b2d3
Eppu Peltonen <eppu.peltonen@icloud.com>
2022-03-15 21:12:21 +0200

    db test

--- controllers/exercises.js
+++ controllers/exercises.js
@@ -1,32 +1,23 @@
 const exerciseRouter = require('express').Router()
 const { Pool } = require('pg')
 
-const pool = (() => {
-  if (process.env.NODE_ENV !== 'production') {
-      return new Pool({
-          connectionString: process.env.DATABASE_URL,
-          ssl: false
-      });
-  } else {
-      return new Pool({
-          connectionString: process.env.DATABASE_URL,
-          ssl: {
-              rejectUnauthorized: false
-            }
-      });
-  } })();
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+})
 
-exerciseRouter.get('/db', async (req, res) => {
+exerciseRouter.get('/', async (req, res) => {
   try {
     const client = await pool.connect()
     const result = await client.query('SELECT * FROM test_table')
     const results = {'results': (result) ? result.rows : null}
-    res.render('pages/db')
+    res.send(results)
     client.release()
   } catch (err) {
     console.error(err)
-    res.send("Error " + err)
+    res.send('Error', err)
   }
 })
+
 
 module.exports = exerciseRouter
