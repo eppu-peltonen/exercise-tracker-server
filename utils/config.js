3e8bf1c8e3abba14568816503001bb20009cb517
Eppu Peltonen <eppu.peltonen@icloud.com>
2022-03-17 14:36:13 +0200

    check if db works locally and remotely

--- utils/config.js
+++ utils/config.js
@@ -3,18 +3,15 @@ const {Pool} = require('pg')
 
 const isProduction = process.env.NODE_ENV === 'production'
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+const ssl = ''
 
-/*
-if (process.env.NODE_ENV === 'production') {
-  connectionString = process.env.DATABASE_URL
-} else if (process.NODE_ENV === 'development') {
-  connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+if (isProduction) {
+  ssl = {rejectUnauthorized: false}
 }
-*/
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: isProduction,
+  ssl: ssl
 })
 
 const PORT = process.env.PORT
