e7741442e3bd11986e204d79a0764eb39c9751ba
Eppu Peltonen <eppu.peltonen@icloud.com>
2022-03-17 14:41:06 +0200

    heroku config

--- utils/config.js
+++ utils/config.js
@@ -3,15 +3,10 @@ const {Pool} = require('pg')
 
 const isProduction = process.env.NODE_ENV === 'production'
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
-const ssl = ''
-
-if (isProduction) {
-  ssl = {rejectUnauthorized: false}
-}
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: ssl
+  ssl: isProduction ? {rejectUnauthorized: false} : null
 })
 
 const PORT = process.env.PORT
